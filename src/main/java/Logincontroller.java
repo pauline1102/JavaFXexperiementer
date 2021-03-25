cfd46298c41a5bf14182c61cd0c9665efa66907a
Williamg91 <william.geismar@gmail.com>
2021-03-25 16:44:42 +0100

    ingen commitment issues her :)))

--- src/main/java/Logincontroller.java
+++ src/main/java/Logincontroller.java
@@ -1,14 +1,20 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
 
 
 import java.io.IOException;
 
 public class Logincontroller {
 
+
+    public TextField CPRfelt;
+    public Label statuslabel;
     @FXML
     TextField tekstfelt;
     @FXML
@@ -37,4 +43,15 @@ public class Logincontroller {
     public void logout() throws IOException {
         StartUp.setRoot("masterlogin");
     }
-}
+
+    public void updateLabel(KeyEvent keyEvent) {
+      String cpr = CPRfelt.getText();
+      if(cpr.length()==11){
+          statuslabel.setText("Søger efter bruger:"+cpr);
+        }
+
+        }
+
+
+    }
+
